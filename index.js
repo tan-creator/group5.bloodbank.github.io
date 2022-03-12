a1b7e830e3446db361d2762b7562a2822735e91d
tuan-anh-0720 <phamanhtuan200420@gmail.com>
2022-03-13 01:54:50 +0700

    Tuan Pham update UI Login

--- index.js
+++ index.js
@@ -3,3 +3,34 @@ const $$ = document.querySelectorAll.bind(document);
 
 // KHAI BÁO BIẾN
 const bloodInventory = $$(".bloodInventory__list-item");
+
+const app = {
+  // Hàm Render ra giao diện
+  render: function () {},
+
+  // Hàm xử lý các sự kiện/chức năng
+  handleEvents: function () {
+    const typeBloods = $$(".descriptionByTypeBlood__item");
+
+    // Xử lý khi click vào các nhóm máu
+    bloodInventory.forEach((inventoryItem, index) => {
+      // Lấy ra trang Description tương ứng dựa vào index
+      const pane = typeBloods[index];
+
+      inventoryItem.onclick = () => {
+        $(".bloodInventory__list-item.active").classList.remove("active");
+        $(".descriptionByTypeBlood__item.active").classList.remove("active");
+
+        inventoryItem.classList.add("active");
+        pane.classList.add("active");
+      };
+    });
+  },
+
+  // Hàm khỏi động khi chạy ứng dụng
+  start: function () {
+    this.handleEvents();
+  },
+};
+
+app.start();
